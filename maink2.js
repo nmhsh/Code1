c14d27d791164f8d3676b86367ab3494a2df5679
nmhsh <110286526+nmhsh@users.noreply.github.com>
2023-06-17 20:46:49 +0700

    Add files via upload

--- maink2.js
+++ maink2.js
@@ -26,26 +26,28 @@ window.key1.click(()=>{
 
 auto()
 let check = false
+let checkSWIPE = false
 function Start(){
   if(currentPackage() != 'com.lyft.android.driver'){
     app.launch('com.lyft.android.driver')
     sleep(2000)
   }
-  let home = id("side_menu_button").findOne(100)
-  if(home) home.click()
-  let scheduled= text("Scheduled Pickup").findOne(100)
-  if(scheduled) click(scheduled.bounds(). centerX(), scheduled.bounds().centerY())
-  let mode = className("android.view.View").depth("16").find()
-  if(mode){
-    for(var i =0;i<mode.length;i++) {
-      let rec = mode[i].bounds()
-      if(rec.text("Airport drop-off").findOne(100)){
-        mode[i].click()
-        alert('Đã click')
+  if(!check){
+    let home = id("side_menu_button").findOne(100)
+    if(home) home.click()
+    let scheduled= text("Scheduled Pickup").findOne(100)
+    if(scheduled){
+      click(scheduled.bounds(). centerX(), scheduled.bounds().centerY())
+      check = true
+    }
+  } else {
+    if(!checkSWIPE) {
+      gestures([0, 500, [440,900], [540,1158]],
+        [0, 500, [640, 1300], [540, 1158]])
+        checkSWIPE = true
+        alert("SWIPE")
         exit()
-        break
-      }
-    };
+    }
   }
 
 }
