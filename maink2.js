2f1da1e84c2c7d3b32e09d46e2ad7126eeeabab8
nmhsh <110286526+nmhsh@users.noreply.github.com>
2023-06-17 20:41:09 +0700

    Add files via upload

--- maink2.js
+++ maink2.js
@@ -27,11 +27,26 @@ window.key1.click(()=>{
 auto()
 let check = false
 function Start(){
-
+  if(currentPackage() != 'com.lyft.android.driver'){
+    app.launch('com.lyft.android.driver')
+    sleep(2000)
+  }
   let home = id("side_menu_button").findOne(100)
   if(home) home.click()
   let scheduled= text("Scheduled Pickup").findOne(100)
   if(scheduled) click(scheduled.bounds(). centerX(), scheduled.bounds().centerY())
+  let mode = className("android.view.View").depth("16").find()
+  if(mode){
+    for(var i =0;i<mode.length;i++) {
+      let rec = mode[i].bounds()
+      if(rec.text("Airport drop-off").findOne(100)){
+        mode[i].click()
+        alert('Đã click')
+        exit()
+        break
+      }
+    };
+  }
 
 }
 
