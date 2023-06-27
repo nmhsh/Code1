a13ecd3c527e1b6162a77ac380867483815e23e5
nmhsh <110286526+nmhsh@users.noreply.github.com>
2023-06-27 15:25:40 +0700

    Add files via upload

--- maink2.js
+++ maink2.js
@@ -1,5 +1,5 @@
+auto()
 let chestSTART = false
-
 let window = floaty.window(
   <frame w='*' h='*'>
     <horizontal>
@@ -12,12 +12,11 @@ window.key.click(()=>{
     if(window.key.getText()=='START'){
       window.key.setText("STOP")
       chestSTART = true
+      START()
       toast("Start")
     } else{
       window.key.setText("START")
       chestSTART = false
-      check = false
-      checkSWIPE = false
       toast("Stop")
     }
 })
@@ -26,43 +25,36 @@ window.key1.click(()=>{
   clearInterval(luong)
   exit()
 })
-
-auto()
-let check = false
-let checkSWIPE = false
-function Start(){
-    let money = id("design_core_map_components_bubble_text").find()
-    let money1 = id("design_core_map_components_bubble_icon").find()
-    let arr =[]
-    if(money){
-      money.forEach(function(i){
-        let str = i.text()
-        if(!str){
-          str = 0
-        } else {
-          str = str.replace ("$","")
-        }
+function START(){
+  let money = id("design_core_map_components_bubble_text").find()
+  let money1 = id("design_core_map_components_bubble_icon").find()
+  let arr =[]
+  if(money){
+    money.forEach(function(i){
+      let str = i.text()
+      if(!str){
+        str = 0
+      } else {
+        str = str.replace ("$","")
+      }
+      arr.push({
+        TIEN : str,
+        region: i.bounds()
+      })
+    })
+    if(money1){
+      money1.forEach(function(i){
         arr.push({
-          TIEN : str,
+          TIEN : false,
           region: i.bounds()
         })
       })
-      if(money1){
-        money1.forEach(function(i){
-          arr.push({
-            TIEN : false,
-            region: i.bounds()
-          })
-        })
-      }
     }
+  }
+  if(arr.length >0){
     log(arr)
-    if(arr.length>0){
-      arr.sort((a,b)=>b.TIEN-a.TIEN)
-      exit()
+  } else {
+    log('ERR')
   }
 }
-console.show()
-let luong = setInterval(()=>{
-  if(chestSTART) Start()
-},50)
+let luong = setInterval(()=>{},0)
